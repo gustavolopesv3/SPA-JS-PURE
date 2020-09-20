9a61699d95d5989fe92d9d13a3012c845cfd7ee8
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2020-09-20 12:27:27 -0500

    refatorando codigo para JS moderno

--- js/script.js
+++ js/script.js
@@ -1,117 +1,126 @@
-window.addEventListener('load', start)
+let globalNames = ['one', 'two', 'three', 'Gustavo Lopes'];
+let inputName = null;
+let currentIndex = null;
+let isEditing = false;
 
-var globalNames = ["Maria do teste", "testevaldo","testeira", "Gustavo Lopes"]; 
-var inputName = null;
-var currentIndex = null;
-var isEditing = false;
+window.addEventListener('load', () => {
+  inputName = document.querySelector('#name');
 
+  preventFormSubmit();
+  activateInput();
+  render();
+});
 
-function start(){
-    inputName = document.querySelector('#name');
+function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+  }
 
-    preventFormSubmit();
-    activateInput();
-    render();
+  let Form = document.querySelector('form');
+  Form.addEventListener('submit', handleFormSubmit);
 }
 
-function preventFormSubmit(){
-    function handleFormSubmit(event){
-        event.preventDefault();
+function activateInput() {
+  function insertName(newName) {
+    //globalNames.push(newName);
+    globalNames = [...globalNames, newName];
+  }
+
+  function updateName(newName) {
+    globalNames[currentIndex] = newName;
+  }
+
+  function handleTyping(e) {
+    var hasText = !!e.target.value && e.target.value.trim() !== '';
+
+    if (!hasText) {
+      clearInput();
+      return;
     }
 
-    let Form = document.querySelector('form');
-    Form.addEventListener('submit', handleFormSubmit);
+    if (e.key == 'Enter') {
+      if (isEditing) {
+        updateName(e.target.value);
+      } else {
+        insertName(e.target.value);
+      }
+
+      render();
+      isEditing = false;
+      clearInput();
+    }
+  }
+
+  inputName.addEventListener('keyup', handleTyping);
+  inputName.focus();
 }
 
-function activateInput(){
-    function insertName(newName){
-        globalNames.push(newName);  
+function render() {
+  function createDeleteButton(index) {
+    function deleteName() {
+      //   //globalNames.splice(index, 1);
+      //   globalNames = globalNames.filter((name, i) => {
+      //     if (i === index) {
+      //       return false;
+      //     }
+      //     return true;
+      //   });
+      globalNames = globalNames.filter((name, i) => i !== index);
+      console.log(i);
+      render();
     }
 
-    function updateName(newName){
-        globalNames[currentIndex] = newName;
+    var button = document.createElement('button');
+    button.textContent = 'x';
+    button.classList.add('deleteButton');
+    button.classList.add('clickable');
+
+    button.addEventListener('click', deleteName);
+
+    return button;
+  }
+
+  function createSpan(name, index) {
+    function editItem() {
+      inputName.value = name;
+      inputName.focus();
+      isEditing = true;
+      currentIndex = index;
     }
 
-    function handleTyping(e){
-        var hasText = !!e.target.value && e.target.value.trim() !== '';
+    var span = document.createElement('span');
+    span.classList.add('clickable');
+    span.textContent = name;
 
-        if(!hasText){
-            clearInput();
-            return;
-        }
+    span.addEventListener('click', editItem);
 
-        if(e.key == "Enter"){
-            if(isEditing){
-                updateName(e.target.value);
-            }else{
-                insertName(e.target.value);
-            }
+    return span;
+  }
 
-            render();
-            isEditing = false;
-            clearInput();
-        }
-    }
+  var divNames = document.querySelector('#names');
+  divNames.innerHTML = '';
 
-    inputName.addEventListener('keyup', handleTyping);
-    inputName.focus();
+  for (i = 0; i < globalNames.length; i++) {
+    var currentName = globalNames[i];
+
+    var li = document.createElement('li');
+    var button = createDeleteButton(i);
+
+    var span = createSpan(currentName, i);
+
+    li.appendChild(button);
+    li.appendChild(span);
+    divNames.appendChild(li);
+    clearImput();
+  }
 }
 
-function render(){
-    function createDeleteButton(index){
-        function deleteName(){
-            globalNames.splice(index,1);
-            render();
-        }
+// function clearInput() {
+//   inputName.value = '';
+//   inputName.focus();
+// }
 
-        var button = document.createElement('button');
-        button.textContent = 'x';
-        button.classList.add('deleteButton');
-        button.classList.add('clickable');
-
-        button.addEventListener('click', deleteName);
-        
-        return button;
-    }
-
-    function createSpan(name,index){
-        function editItem(){
-            inputName.value = name;
-            inputName.focus();
-            isEditing = true;
-            currentIndex = index;
-        }
-
-        var span = document.createElement('span');
-        span.classList.add('clickable');
-        span.textContent = name;
-
-        span.addEventListener('click', editItem);
-
-        return span;
-    }
-    
-    var divNames = document.querySelector('#names');
-    divNames.innerHTML = '';
-
-    for(i= 0 ; i < globalNames.length; i++){
-        
-        var currentName = globalNames[i]
-
-        var li = document.createElement('li');
-        var button = createDeleteButton(i);
-
-        var span = createSpan(currentName, i);
-
-        li.appendChild(button)
-        li.appendChild(span)
-        divNames.appendChild(li);
-        
-    }
-}
-
-function clearInput(){
-    inputName.value = '';
-    inputName.focus();
-}
-
+const clearImput = () => {
+  inputName.value = '';
+  inputName.focus();
+};
